a47a9283bf6cd5503fdfc6918aabc620fcb2953b
Vivek Pareek <vivekpareek096@gmail.com>
2024-02-01 18:26:21 +0530

    add extra solutions to Count digits

--- a2z/src/main/java/org/dsa/step1/one_4_basicmath/CountDigits.java
+++ a2z/src/main/java/org/dsa/step1/one_4_basicmath/CountDigits.java
@@ -1,12 +1,25 @@
 package org.dsa.step1.one_4_basicmath;
 
+import static org.dsa.util.Util.*;
+
 /**
  * Count digits in a number
  * Problem Statement: Given an integer N, write a program to count the number of digits in N.
  */
 public class CountDigits {
 
-    public static int getDigitCount(int number) {
+    public static int getDigitCountIteration(int number) {
+        int count = 0;
+        //have to account for negative numbers
+        number = number < 0 ? -number : number;
+        //divide by ten's to find count of digits excluding last oneth place
+        for(;number > 0; number/=10) {
+            count++;
+        }
+        return count;
+    }
+
+    public static int getDigitCountRecursion(int number) {
         //have to account for negative numbers
         number = number < 0 ? -number : number;
         //single digit number return 1 already
@@ -15,14 +28,53 @@ public class CountDigits {
         }
         //divide by ten's to find count of digits excluding last oneth place
         int q = number/10;
-        return 1 + getDigitCount(q);
+        return 1 + getDigitCountRecursion(q);
+    }
+
+    public static int getDigitCountStringConvert(int number) {
+        number = number < 0 ? -number : number;
+        return Integer.toString(number).length();
+    }
+
+    public static int getDigitCountLongBase10(int number) {
+        number = number < 0 ? -number : number;
+        return (int) Math.floor(Math.log10(number)) + 1;
     }
 
     public static void main(String[] args) {
-        System.out.println(getDigitCount(1000));
-        System.out.println(getDigitCount(-1000));
-        System.out.println(getDigitCount(1005));
-        System.out.println(getDigitCount(2));
-        System.out.println(getDigitCount(22325));
+        println("--------------------------------------------------------");
+        println("Recursion Solution");
+        println(getDigitCountRecursion(1000));
+        println(getDigitCountRecursion(-1000));
+        println(getDigitCountRecursion(1005));
+        println(getDigitCountRecursion(2));
+        println(getDigitCountRecursion(22325));
+
+        println("--------------------------------------------------------");
+        println("Iteration Solution");
+
+        println(getDigitCountIteration(1000));
+        println(getDigitCountIteration(-1000));
+        println(getDigitCountIteration(1005));
+        println(getDigitCountIteration(2));
+        println(getDigitCountIteration(22325));
+
+        println("--------------------------------------------------------");
+        println("String length Solution");
+
+        println(getDigitCountStringConvert(1000));
+        println(getDigitCountStringConvert(-1000));
+        println(getDigitCountStringConvert(1005));
+        println(getDigitCountStringConvert(2));
+        println(getDigitCountStringConvert(22325));
+
+        println("--------------------------------------------------------");
+        println("log10 Solution");
+
+        println(getDigitCountLongBase10(1000));
+        println(getDigitCountLongBase10(-1000));
+        println(getDigitCountLongBase10(1005));
+        println(getDigitCountLongBase10(2));
+        println(getDigitCountLongBase10(22325));
     }
 }
---
+++ a2z/src/main/java/org/dsa/util/Util.java
@@ -0,0 +1,11 @@
+package org.dsa.util;
+
+public class Util {
+    public static void println(Object obj) {
+        System.out.println(obj);
+    }
+
+    public static void print(Object obj) {
+        System.out.print(obj);
+    }
+}
